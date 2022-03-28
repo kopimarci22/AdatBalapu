356243895660aebeefa96ee43ea475144921a3fb
KopiMarci <kopanecz99@gmail.com>
2022-03-28 21:45:19 +0200

    Csatlakozás kezdemények
    Csatlakozás az adatbázishoz és kiiratás kezdemények

---
+++ Code/tablak.php
@@ -0,0 +1,31 @@
+<?php
+
+$conn = oci_connect('DAVID', 'asd123', 'localhost/XE');
+if (!$conn) {
+    $e = oci_error();
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+
+print ("User tabla");
+$stid = oci_parse($conn, 'SELECT * FROM FELHASZNALO');
+if(!$stid) {
+    $e = oci_error($conn);
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+$r = oci_execute($stid);
+if(!$r){
+    $e = oci_error($stid);
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+print "<table border='1'>\n";
+while($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+    print "<tr>\n";
+    foreach ($row as $item) {
+        print "<td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+    }
+    print "</tr>\n";
+}
+print "</table><br>\n";
+oci_free_statement($stid);
+
+?>
