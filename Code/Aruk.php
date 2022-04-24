9032d8dc3f7bf196a9306f440c86dfcf8e2644b7
nagydavi <nagyfalusidavid@gmail.com>
2022-04-24 19:16:48 +0200

    Adatmanipulációs oldalak ui alapok

---
+++ Code/Aruk.php
@@ -0,0 +1,29 @@
+<?php?>
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <title>Főoldal</title>
+    <link rel="stylesheet" href="All.css"/>
+</head>
+<body>
+<div id="helpdiv"><nav><ul id="menu">
+            <li class="lik"><a href="Fooldal.php" class="lika" style="color: black">Főoldal</a></li>
+            <li class="lik"><a href="Aruk.php" class="lika" style="color: blueviolet">Áruk</a></li>
+            <li class="lik"><a href="Kosar.php" class="lika" style="color: black">Kosár</a></li>
+            <li class="lik"><a href="Login.php"  class="lika" style="color: black">Login</a></li>
+            <li class="lik"><a href="Regist.php"  class="lika" style="color: black">Registration</a></li>
+        </ul></nav>
+</div>
+<div>
+    <p>Édesség</p>
+    <p>Üdítő</p>
+    <p>Pékáru</p>
+    <p>Alkoholos italok</p>
+    <p>Húsok</p>
+    <p>Tejtermékek</p>
+    <p>Kert</p>
+    <p>Férfi ruházat</p>
+    <p>Női ruházat</p>
+</div>
+</html>
--- Code/Regist.php
+++ Code/Regist.php
@@ -1,1 +1,30 @@
-<?php
+
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <title>Regisztráció</title>
+    <link rel="stylesheet" href="All.css"/>
+</head>
+<body>
+<div id="prob"><form id="belep" action="Regist.php" method="post" enctype="multipart/form-data" autocomplete="off">
+        <fieldset id="regist" style="height: 500px">
+            <legend>Regisztráció</legend>
+            <label>Felhasználónév: <input type="text" name="fnev"  placeholder="Felhasználónév..." maxlength="10" class="registinput"/></label><br/>
+            <label>Jelszó: <input type="password" name="pass" maxlength="10" placeholder="**********" class="registinput" /></label><br/>
+            <label>Jelszó ismétlés: <input type="password" name="pass2" maxlength="10" placeholder="**********" class="registinput" /></label><br/>
+            <label>Lakcím: <input type="text" name="lakcim"  class="registinput" required/></label><br/>
+            <label>Születésnap: <input type="date" name="szuldatum"  class="registinput" required/></label><br/>
+            <label>E-mail cím: <input type="email" name="email" placeholder="valami@gmail.com"  class="registinput"/></label><br/>
+            <label>Bankkártya: <input type="text" name="email"class="registinput"/></label><br/>
+            <input type="submit" name="regist" value="Regisztráció" id="registgomb">
+        </fieldset>
+    </form></div>
+<div>
+    <p id="belepszoveg">Van már fiókod?<a href="Login.php">Bejelentkezés</a><br/>
+    <a href="Fooldal.php" id="vissza">Vissza</a></p>
+</div>
+
+
+</body>
+</html>
