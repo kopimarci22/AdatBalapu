6c3631a062a956b30f3166067a14dfafa886d050
KopiMarci <kopanecz99@gmail.com>
2022-04-24 22:37:04 +0200

    bejelentkezés html+php

--- Code/Login.php
+++ Code/Login.php
@@ -1,25 +1,32 @@
+<?php session_start(); ?>
 <?php
+include "databaseConnection.php";
+$con = DBconnection::getInstance();//connection string
+if (!$con->getConnection()) {
+    $m = oci_error();
+    echo $m['message'], "\n";
+}
+$query = "select fel_nev, jelszo from user where fel_nev = :username and jelszo= :password";
+$stid = oci_parse($con->getConnection(), $query);
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+        echo $query;
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    oci_bind_by_name($stid, ":username", $username);
+    oci_bind_by_name($stid, ":password", $password);
+    oci_execute($stid);
+    $row = oci_fetch_array($stid, OCI_ASSOC);
+    if ($row = 1) {
+        $_SESSION['username'] = $username;
+        header("Location: Fooldal.php");
+    } else {
+        echo "Wrong username or password";
+    }
+    oci_free_statement($stid);
+    oci_close($con->getConnection());
+
+}
 
 ?>
-<!DOCTYPE HTML>
-<html lang="hu">
-<head>
-    <title>KiPa bejelentkezés</title>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="">
-</head>
-<body>
-<header><i>KiPa</i> bejelentkezés</header>
-<div class="content">
-    <form action="../script/php/handleLogin.php" method="post">
-        <h2>Bejelentkezés</h2>
-        <input type="text" name="userEmail" placeholder="Email"><br>
-        <input type="password" name="password" placeholder="jelszó"><br>
-        <input type="submit" value="Bejelentkezés">
-        <label>Admin</label><input type="checkbox" name="adminSelect" value="admin">
-    </form>
-    <a href="regisztracio.html">Még nincs fiókja?</a>
-</div>
-<footer>Kopanecz Márton és Kiss Máté honlapja</footer>
-</body>
-</html>
+
---
+++ Code/databaseConnection.php
@@ -0,0 +1,30 @@
+<?php
+class DBconnection
+{
+    public static $conn = null;
+    private static $instance = null;
+
+    private function __construct()
+    {
+        $connection = oci_connect('DAVID', 'asd123', 'localhost/XE', "UTF8") or die("Hibás csatlakozás!");
+        if (!$connection) {
+            $m = oci_error();
+            echo $m['message'], "\n";
+            exit;
+        } else {
+        }
+        self::$conn = $connection;
+    }
+
+    public function getConnection()    {
+        return self::$conn;
+    }
+
+
+    public static function getInstance()    {
+        if (self::$instance == null) {
+            self::$instance = new DBConnection();
+        }
+        return self::$instance;
+    }
+}
