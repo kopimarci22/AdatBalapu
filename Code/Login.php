35e885f77b440855416ea1d39b434cf8ea38d277
KopiMarci <kopanecz99@gmail.com>
2022-04-24 18:41:26 +0200

    bejelentkezés css

--- Code/Login.php
+++ Code/Login.php
@@ -1,25 +1,20 @@
-<?php
-
-?>
 <!DOCTYPE HTML>
 <html lang="hu">
 <head>
-    <title>KiPa bejelentkezés</title>
+    <title>SPARta Bejelentkezés</title>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="">
+    <link rel="stylesheet" type="text/css" href="All.css">
 </head>
 <body>
-<header><i>KiPa</i> bejelentkezés</header>
-<div class="content">
-    <form action="../script/php/handleLogin.php" method="post">
-        <h2>Bejelentkezés</h2>
-        <input type="text" name="userEmail" placeholder="Email"><br>
-        <input type="password" name="password" placeholder="jelszó"><br>
-        <input type="submit" value="Bejelentkezés">
-        <label>Admin</label><input type="checkbox" name="adminSelect" value="admin">
+<header><a href="Fooldal.php">Vissza a főoldalra</a></header>
+<div class="content" >
+    <form action="" method="post">
+        <h2 class="text-center" >Bejelentkezés</h2>
+        <input class="input" type="text" name="userEmail" placeholder="Email"><br>
+        <input class="input" type="password" name="password" placeholder="jelszó"><br>
+        <input class="input" type="submit" value="Bejelentkezés">
     </form>
-    <a href="regisztracio.html">Még nincs fiókja?</a>
+    <a href="Regist.php">Még nincs fiókja?</a>
 </div>
-<footer>Kopanecz Márton és Kiss Máté honlapja</footer>
 </body>
 </html>
